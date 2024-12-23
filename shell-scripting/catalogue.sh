c1ad262d7008503ddf4cdb8af027dcebcb4d6a19
lenovo <asifnadeem2668@gmail.com>
2024-12-24 05:03:36 +0530

    First commit

--- shell-scripting/catalogue.sh
+++ shell-scripting/catalogue.sh
@@ -19,3 +19,8 @@ systemctl daemon-reload
 
 systemctl enable catalogue
 systemctl start catalogue
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+dnf install mongodb-mongosh -y
+
+mongosh --host MONGODB-SERVER-IPADDRESS </app/db/master-data.js
